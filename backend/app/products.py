d7e334e06bdf54ffb20a6b3d6a7f71a3f78b7690
Ramuzikkk <fd702144@gmail.com>
2026-07-15 22:47:33 +0300

    Create products.py

---
+++ backend/app/products.py
@@ -0,0 +1,8 @@
+PRODUCTS = [
+    {"id": 1, "name": "60 UC", "price": 99},
+    {"id": 2, "name": "325 UC", "price": 449},
+    {"id": 3, "name": "660 UC", "price": 899},
+    {"id": 4, "name": "1800 UC", "price": 2299},
+    {"id": 5, "name": "3850 UC", "price": 4499},
+    {"id": 6, "name": "8100 UC", "price": 8999},
+]
