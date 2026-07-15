c0cb546c9686764bc4fbc1c281f70e3969c7302a
Ramuzikkk <fd702144@gmail.com>
2026-07-15 21:50:59 +0300

    Create models.py

---
+++ backend/app/models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, BigInteger, String
+from .database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    telegram_id = Column(BigInteger, unique=True)
+    username = Column(String(255))
