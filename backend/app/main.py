5a4e0b62411063d8396ac4e1dc9d62a4c894b1ba
Ramuzikkk <fd702144@gmail.com>
2026-07-15 21:52:10 +0300

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from .database import engine, Base
+
 
 app = FastAPI(
     title="ZULMACH UC BOT API",
@@ -6,16 +8,15 @@ app = FastAPI(
 )
 
 
+@app.on_event("startup")
+async def startup():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
 @app.get("/")
 def home():
     return {
         "status": "ok",
         "project": "ZULMACH UC BOT"
-    }
-
-
-@app.get("/health")
-def health():
-    return {
-        "status": "healthy"
     }
